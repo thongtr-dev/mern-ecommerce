9611d7883cc996c4136121a65cbe5b1d1b37bb37
thongtr-dev <thongtr.dev@gmail.com>
2024-04-02 14:27:42 +0700

    BE: Add product controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,21 +1,13 @@
 import express from 'express';
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
-  console.log(`Returning ${products.length} products`);
-});
+router.route('/').get(getProducts);
 
-router.get('/:id', async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  if (product) {
-    return res.json(product);
-  }
-  res.status(404).json({ message: 'Product not found' });
-});
+router.route('/:id').get(getProductById);
 
 export default router;
