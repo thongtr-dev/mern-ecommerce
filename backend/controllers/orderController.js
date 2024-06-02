9c43d952f6e8472125a3cd45c7314087717a143c
thongtr-dev <thongtr.dev@gmail.com>
2024-06-02 15:49:22 +0700

    feat: BE add addOrderItems Order controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,38 @@ import Order from '../models/orderModel.js';
  * @access Private
  */
 const addOrderItems = asyncHandler(async (req, res) => {
-  res.send('add order items');
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error('No order items');
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((x) => ({
+        ...x,
+        product: x._id,
+        _id: undefined,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = order.save();
+    res.status(201).json(createdOrder);
+  }
 });
 
 /**
