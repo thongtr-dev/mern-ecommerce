947d4448da5c979a8a9241d047c266a86cad2bd9
thongtr-dev <thongtr.dev@gmail.com>
2024-06-03 11:31:01 +0700

    feat: BE add getMyOrders and getOrderById controllers
    This commit also addresses the module import bug in orderRoutes.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -43,11 +43,12 @@ const addOrderItems = asyncHandler(async (req, res) => {
 
 /**
  * @desc Get logged in user orders
- * @route GET /api/orders/myorders
+ * @route GET /api/orders/mine
  * @access Private
  */
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.send('get my orders');
+  const orders = await Order.find({ user: req.user_id });
+  res.status(200).json(orders);
 });
 
 /**
@@ -56,7 +57,17 @@ const getMyOrders = asyncHandler(async (req, res) => {
  * @access Private/Admin
  */
 const getOrderById = asyncHandler(async (req, res) => {
-  res.send('get order by id');
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
 });
 
 /**
