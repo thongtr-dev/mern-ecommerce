cf1f4295da4362dea88be0dc5dbd4f647b50cb50
thongtr-dev <thongtr.dev@gmail.com>
2024-06-05 13:05:44 +0700

    feat: FE add get all orders api slice

--- frontend/src/screens/admin/OrderListScreen.jsx
+++ frontend/src/screens/admin/OrderListScreen.jsx
@@ -2,8 +2,11 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button } from 'react-bootstrap';
 import { FaTimes } from 'react-icons/fa';
 import Message from '../../components/Message';
+import { useGetOrdersQuery } from '../../slices/ordersApiSlice';
 
 const OrderListScreen = () => {
+  const { data: orders, isLoading, error } = useGetOrdersQuery();
+
   return <div>OrderListScreen</div>;
 };
 
