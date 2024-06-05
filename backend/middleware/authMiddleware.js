442ab235b52e2c9f4626a6e510e6b2e83a966f90
thongtr-dev <thongtr.dev@gmail.com>
2024-06-05 12:57:39 +0700

    fix: BE fix bug auth middleware, wrap admin middleware in asyncHandler

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -26,13 +26,13 @@ const protect = asyncHandler(async (req, res, next) => {
 });
 
 // Admin middleware
-const admin = () => (req, res, next) => {
+const admin = asyncHandler(async (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
   } else {
     res.status(401);
     throw new Error('Not authorized as admin');
   }
-};
+});
 
 export { protect, admin };
