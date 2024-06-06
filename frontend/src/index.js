4e94c531ebb43742ccaccfe51f2effe5189aa2dc
thongtr-dev <thongtr.dev@gmail.com>
2024-06-06 10:20:26 +0700

    feat: FE add admin Product List screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -25,6 +25,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import OrderListScreen from './screens/admin/OrderListScreen';
+import ProductListScreen from './screens/admin/ProductListScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -45,6 +46,7 @@ const router = createBrowserRouter(
 
       <Route path='' element={<AdminRoute />}>
         <Route path='/admin/order-list' element={<OrderListScreen />} />
+        <Route path='/admin/product-list' element={<ProductListScreen />} />
       </Route>
     </Route>
   )
---
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -0,0 +1,75 @@
+import { LinkContainer } from 'react-router-bootstrap';
+import { Table, Button, Row, Col } from 'react-bootstrap';
+import { FaTimes, FaEdit, FaTrash } from 'react-icons/fa';
+import Message from '../../components/Message';
+import Loader from '../../components/Loader';
+import { useGetProductsQuery } from '../../slices/productsApiSlice';
+
+const ProductListScreen = () => {
+  const { data: products, isLoading, error } = useGetProductsQuery();
+
+  const deleteHandler = (id) => {};
+
+  return (
+    <>
+      <Row className='align-items-center'>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className='text-end'>
+          <Button className='btn-sm m-3'>
+            <FaEdit /> Create Product
+          </Button>
+        </Col>
+      </Row>
+
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>
+          {error?.data?.message || error.message}
+        </Message>
+      ) : (
+        <>
+          <Table striped hover responsive className='table-sm'>
+            <thead>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>PRICE</th>
+              <th>CATEGORY</th>
+              <th>BRAND</th>
+              <th></th>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product._id}>
+                  <td>{product._id}</td>
+                  <td>{product.name}</td>
+                  <td>{product.price}</td>
+                  <td>{product.category}</td>
+                  <td>{product.brand}</td>
+                  <td>
+                    <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                      <Button variant='light' className='btm-sm mx-2'>
+                        <FaEdit />
+                      </Button>
+                    </LinkContainer>
+                    <Button
+                      variant='danger'
+                      className='btn-sm'
+                      onClick={() => deleteHandler(product._id)}
+                    >
+                      <FaTrash />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
+    </>
+  );
+};
+
+export default ProductListScreen;
