6dc132e4155ccc3af2357d93b561287041f530a1
thongtr-dev <thongtr.dev@gmail.com>
2024-06-07 17:02:10 +0700

    feat: BE install multer and add upload product image functionality

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,56 @@
+import path from 'path';
+import express from 'express';
+import multer from 'multer';
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+function fileFilter(req, file, cb) {
+  const fileTypes = /jpe?g|png|webp/;
+  const mimeTypes = /image\/jpe?g|image\/png|image\/webp/;
+
+  const extName = fileTypes.test(
+    path.extname(file.originalname).toLocaleLowerCase()
+  );
+  console.log(extName);
+  const mimeType = mimeTypes.test(file.mimetype);
+  console.log(mimeType);
+
+  if (extName && mimeType) {
+    cb(null, true);
+  } else {
+    cb(new Error('Image only!'), false);
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter,
+});
+
+const uploadSingleImage = upload.single('image');
+
+router.route('/').post((req, res) => {
+  uploadSingleImage(req, res, function (err) {
+    if (err) {
+      return res.status(400).send({ message: err.message });
+    }
+
+    res.status(200).send({
+      message: 'Image uploaded successfully',
+      image: `/${req.file.path}`,
+    });
+  });
+});
+
+export default router;
