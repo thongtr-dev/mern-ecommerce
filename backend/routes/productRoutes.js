5ff8d4cfb7c30fd1c8d908b06e8467e410f659fc
thongtr-dev <thongtr.dev@gmail.com>
2024-06-11 16:36:52 +0700

    feat: FE BE add home page product carousel

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,6 +6,7 @@ import {
   updateProduct,
   deleteProduct,
   createProductReview,
+  getTopProducts,
 } from '../controllers/productController.js';
 
 import { protect, admin } from '../middleware/authMiddleware.js';
@@ -13,6 +14,7 @@ import { protect, admin } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/top').get(getTopProducts);
 
 router
   .route('/:id')
