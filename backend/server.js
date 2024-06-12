9dcdf97aededb476c38db0d3d462f4ffd41a1c9c
thongtr-dev <thongtr.dev@gmail.com>
2024-06-12 14:34:35 +0700

    fix: BE catch all server requests and redirect to index.html

--- backend/server.js
+++ backend/server.js
@@ -4,27 +4,21 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 
-connectDB(); // Connect to MongoDB
+connectDB();
 
 const app = express();
 
-// Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-app.use((req, res, next) => {
-  console.log(`Request URL: http://localhost:${process.env.PORT}${req.url}`);
-  next();
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -35,8 +29,8 @@ app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
+const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
   app.use('/uploads', express.static('/var/data/uploads'));
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
@@ -44,16 +38,15 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 } else {
-  const __dirname = path.resolve();
   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.get('/', (req, res) => res.send('API is running...'));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
 }
 
 app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () =>
-  console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${port}`
-  )
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
 );
